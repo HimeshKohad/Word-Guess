eb68a5fe490b7962ce92466b618c889af6b31c14
Himesh Kohad <107066424+HimeshKohad@users.noreply.github.com>
2023-08-18 14:22:07 +0530

    Update GameOver.js

--- src/components/GameOver.js
+++ src/components/GameOver.js
@@ -27,12 +27,30 @@ function GameOver() {
         Correct Word: {correctWord.toUpperCase()}
       </h1>
       {gameOver.guessedWord && (
-        <h3
-          style={{ fontFamily: "Poppins" }}
-        >
+        <h3 style={{ fontFamily: "Poppins" }}>
           You guessed in {currAttempt.attempt} attempts
         </h3>
       )}
+      
+      {/* New Game Button */}
+      <button
+        style={{
+          fontFamily: "Poppins",
+          marginTop: "20px",
+          padding: "15px 30px",
+          backgroundColor: "#b49f39",
+          color: "white",
+          border: "none",
+          cursor: "pointer",
+          fontWeight: "bold",
+          fontSize: "24px",
+          borderRadius: "8px", 
+          boxShadow: "0px 3px 6px rgba(0, 0, 0, 0.1)", 
+        }}
+        onClick={() => window.location.reload()}
+      >
+        New Game
+      </button>
     </div>
   );
 }
